edb6da5141ff5f9ff20577c32e01256d854aa52a
Alejandro Diaz <alejandro.diaz@axians.es>
2022-09-09 14:50:57 +0200

    REMOVE unused class method, add initializer

--- logic/image_download.py
+++ logic/image_download.py
@@ -3,19 +3,22 @@ import re
 
 # from config.properties import PROMPT_REGEX, URL_REGEX
 
-PROMPT_REGEX = "(?<=Prompt: )(.*?)(?=\\v)"
+PROMPT_REGEX = "(?<=\")(.*?)(?=\")"
 
 URL_REGEX = "(?<=')(.*?)(?=')"
 
 
 class Download:
-    def read_log_file():
+    def __init__(self) -> None:
+        pass
+
+    def read_log_file(self):
         with open('./predictions.log', 'r') as log_file:
             file_content = log_file.read()
             return file_content
 
-    def filter_regex(regex: str):
-        file_content = Download.read_log_file()
+    def filter_regex(self, regex: str):
+        file_content = self.read_log_file()
         filelines = file_content.splitlines()
         filtered_list = []
         for line in filelines:
@@ -25,23 +28,14 @@ class Download:
                 filtered_list.append(match_str)
         return filtered_list
 
-    def get_file_name():
-        names_list = Download.filter_regex(PROMPT_REGEX)
-        for name in names_list:
-            print(name)
-
-    def download_imgs():
-        urls_list = Download.filter_regex(URL_REGEX)
-        counter = 0
+    def download_imgs(self):
+        urls_list = self.filter_regex(URL_REGEX)
+        # names_list = self.filter_regex(PROMPT_REGEX)
         for url in urls_list:
             img_data = requests.get(url).content
-            counter += 1
-            with open(f"./images/{counter}.jpg", 'wb') as handler:
+            with open("./images/image.jpg", 'wb') as handler:
                 handler.write(img_data)
 
 
 if __name__ == "__main__":
-    Download.read_log_file()
-    Download.filter_regex(URL_REGEX)
     Download.download_imgs()
-

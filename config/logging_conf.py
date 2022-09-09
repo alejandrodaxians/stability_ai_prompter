1421a4626467533d448f4c9a3308ba400fb0b78d
Alejandro Diaz <alejandro.diaz@axians.es>
2022-09-09 14:48:56 +0200

    UPDATE according to linter

--- config/logging_conf.py
+++ config/logging_conf.py
@@ -1,8 +1,9 @@
 import logging
 
+
 def config_logs():
-    logging.basicConfig(filename="predictions.log", 
+    logging.basicConfig(filename="predictions.log",
                         encoding='utf-8',
                         level=logging.INFO,
-                        format='%(asctime)s %(levelname)-8s %(message)s', 
+                        format='%(asctime)s %(levelname)-8s %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S')

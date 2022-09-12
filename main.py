e4feb8bb620ef40c968d0bd7156812c64763bb15
Alejandro Diaz <alejandro.diaz@axians.es>
2022-09-12 12:53:15 +0200

    ADD download and renaming flow

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from config.logging_conf import config_logs
 from logic.replicate import StabilityAI
+from logic.image_download import Download
+from logic.rename_image import Rename
 
 
 stability = StabilityAI()
+download = Download()
+rename = Rename()
 
 
 if __name__ == "__main__":
     config_logs()
     stability.log_output_url()
+    download.download_imgs()
+    rename.rename_file()

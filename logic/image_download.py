494997c8c7142847addca1c0e870d0b834e11faf
Alejandro Diaz <alejandro.diaz@axians.es>
2022-09-14 16:58:06 +0200

    REMOVE log file reading, UPDATE functions to read variables

--- logic/image_download.py
+++ logic/image_download.py
@@ -3,25 +3,16 @@ import requests
 import re
 
 from config.properties import URL_REGEX
+from logic.replicate import StabilityAI
+
+stability = StabilityAI()
 
 
 class Download:
     def __init__(self) -> None:
         pass
 
-    def read_log_file(self):
-        """
-        Searchs for the specified log file and reads its content.
-
-        **params**: None
-
-        **returns**: A string with all the log file's content.
-        """
-        with open('./predictions.log', 'r') as log_file:
-            file_content = log_file.read()
-            return file_content
-
-    def filter_regex(self, regex: str) -> List:
+    def filter_url(self) -> List:
         """
         Reads the log file returned by read_log_file()
         and filters it according to a regex.
@@ -31,17 +22,11 @@ class Download:
 
         **returns**: A list with each match from the regex as each indexed value.
         """
-        file_content = self.read_log_file()
-        filelines = file_content.splitlines()
-        filtered_list = []
-        for line in filelines:
-            regex_match = re.search(regex, line)
-            if regex_match:
-                match_str = regex_match.group(0)
-                filtered_list.append(match_str)
-        return filtered_list
+        url = str(stability.get_output_url())
+        regex_match = re.search(URL_REGEX, url)
+        return regex_match.group(0)
 
-    def download_imgs(self) -> None:
+    def download_img(self) -> None:
         """
         Downloads every image hosted on the valid urls that it
         finds inside the filtered list returned by filter_regex().
@@ -50,11 +35,9 @@ class Download:
 
         **returns**: None.
         """
-        urls_list = self.filter_regex(URL_REGEX)
-        counter = 0
-        for url in urls_list:
-            counter += 1
-            img_name = "./images/" + str(counter) + ".jpg"
-            img_data = requests.get(url).content
-            with open(img_name, 'wb') as handler:
-                handler.write(img_data)
+        url = self.filter_url()
+        prompt_name = stability.prompt
+        img_name = f"./images/{prompt_name}.jpg"
+        img_data = requests.get(url).content
+        with open(img_name, 'wb') as handler:
+            handler.write(img_data)
